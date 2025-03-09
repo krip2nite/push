8cf61ff23c74f4c683326ac648e0a32e222f03fa
krip2nite <kochnevandrey94@gmail.com>
2025-03-09 19:00:40 +0200

    arrow function cannot be a method

--- lesson19/mergeObjects.js
+++ lesson19/mergeObjects.js
@@ -5,4 +5,6 @@ export function mergeObjects(firstObj, secondObj, isFirstPref)
     // if there is conflict of keys the parameter isFirstPref defines how to resolve the conflict
     // if isFirstPref is true than result object should contain value of the conflicted key from the first object
     // otherwise from the second one.
+    return isFirstPref ? {...secondObj, ...firstObj} : {...firstObj, ...secondObj}
 }
+// const obj x = {x:2 , x:3} /// x=3 one object
---
+++ lesson20/index.js
@@ -0,0 +1,7 @@
+let first = 1;
+let second = 2;
+[first, second] = [second, first]
+console.log(`second  = ${second}`)
+console.log(`first = ${first}`)
+
+// swap between 2 parameters **interview** question
