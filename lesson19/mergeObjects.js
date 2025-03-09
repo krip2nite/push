2cd5878bafc85bfc52b9ece6d37c0f890de35d3e
krip2nite <kochnevandrey94@gmail.com>
2025-03-09 15:52:23 +0200

    first try

---
+++ lesson19/mergeObjects.js
@@ -0,0 +1,8 @@
+export function mergeObjects(firstObj, secondObj, isFirstPref)
+{
+    // TODO
+    // returns result object as merging of firstObj and secondObj
+    // if there is conflict of keys the parameter isFirstPref defines how to resolve the conflict
+    // if isFirstPref is true than result object should contain value of the conflicted key from the first object
+    // otherwise from the second one.
+}
