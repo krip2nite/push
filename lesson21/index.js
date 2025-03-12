eae96afc49a4dd912f53c3f7ece0e108ac8f6cec
krip2nite <kochnevandrey94@gmail.com>
2025-03-12 20:50:59 +0200

    demo of "call" and "bind" methods

--- lesson21/index.js
+++ lesson21/index.js
@@ -1,14 +1,31 @@
 import { testframework } from "./testframework.js";
-function f1() {
-    return "kuku";
+class X {
+    constructor(arr){
+        this.arr = arr;
+    }
+    logElements(){
+        this.arr.forEach(e => console.log(e));
+    }
+    sumElements(){
+        return this.arr.reduce((acc,cur) => acc + cur);
+    }
+    action(fun){
+        return fun.call(this);
+    }
+    joinElements(){
+        return this.arr.join();
+    }
+    actionBind(fun){
+        return fun();
+    }
 }
-function sum(op1 = 10, op2 = 20) {
-    return op1 + op2;
-}
-Function.prototype.toString = function(){
-    return this().toString();
-}
-// console.log(f1.toString());
-// console.log(sum.toString());
-console.log(`sum.call(undifined, 1 ,2) = ${sum.call(undefined, 1 ,2)}`)
-console.log(`sum.apply(undifined, [1 ,2]) = ${sum.apply(undefined, [1 ,2])}`)
+const obgX = new X([1,2,3,20,40]);
+// obgX.logElements();
+// console.log(obgX.sumElements());
+// obgX.action(obgX.logElements);
+// console.log(obgX.action(obgX.sumElements));
+// console.log(obgX.action(function(){
+//     return this.arr.join();
+// }));
+console.log(obgX.action(obgX.joinElements));
+console.log(obgX.actionBind(obgX.sumElements.bind(obgX)));
