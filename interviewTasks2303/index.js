0b2c3dd2e96c54d5e5bda3ea41b6f1fc08462722
krip2nite <kochnevandrey94@gmail.com>
2025-03-23 21:02:23 +0200

    var with setTimeout context

--- interviewTasks2303/index.js
+++ interviewTasks2303/index.js
@@ -1,3 +1,14 @@
-'use strict'
-a = 10;
-console.log('a',a);
+function fun1(){
+    var a = 40;
+    if (a == 40){
+        var a= 10;
+        setTimeout((b) => console.log('a',b), 0, a);
+        // sending the 'a' as an argument at the end of the function
+        // the function gets the b(parameter) in that moment of the code and will print 10
+    }
+    a = 30;
+}
+
+
+
+fun1();
